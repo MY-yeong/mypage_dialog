bef38958632eddd2e87426210378de3a932747df
Kminyeong <minyeong3@daum.net>
2023-08-12 00:18:47 +0900

    dialog

--- app/src/main/java/com/example/mypage/MainActivity.kt
+++ app/src/main/java/com/example/mypage/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import com.google.android.material.switchmaterial.SwitchMaterial
 import kotlin.String
@@ -12,8 +14,8 @@ import kotlin.Unit
 
 class MainActivity : AppCompatActivity() {
 
-//    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
-
+    val TAG: String = "로그"
+    //    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,17 +41,19 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val logout : Button = findViewById(R.id.logout)
-        logout.setOnClickListener {
-            val intent = Intent(this, sub::class.java)
-            startActivity(intent)
-        }
+    }
 
-        val del : Button = findViewById(R.id.del)
-        del.setOnClickListener {
-            val intent = Intent(this, pays::class.java)
-            startActivity(intent)
-        }
+    fun onDialogBtnClicked(view: View){
+        Log.d(TAG, "MainActivity - onDialogBtnClicked() called")
+
+        val myCustomDialog = MyCustomDialog(context = this)
+        myCustomDialog.show()
+    }
+
+    fun onDialogBtn2Clicked(view: View){
+        Log.d(TAG, "MainActivity - onDialogBtnClicked() called")
+        val myCustomDialog2 = MyCustomDialog2(context = this)
+        myCustomDialog2.show()
     }
 
     companion object {
